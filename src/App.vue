ed1fef6a3050a6f7c9c2661d4858d450b9ed194e
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2022-10-07 22:07:28 +0200

    basic changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script stetup>
-import { triggerRef } from "@vue/reactivity";
 export default {
   data() {
     return {
@@ -38,7 +37,11 @@ export default {
       if (this.message == correctAnswer || this.message == correctAnswer.toUpperCase()) {
         return "Answer is correct!";
       } else {
-        return `Answer isn't correct! The correct answer was ${this.rgbToHex(this.r, this.g, this.b)}`;
+        return `Answer isn't correct! The correct answer was ${this.rgbToHex(
+          this.r,
+          this.g,
+          this.b
+        )}`;
       }
     },
   },
@@ -50,7 +53,15 @@ export default {
     <div class="Color"></div>
     <h1 v-if="!start">RGB To HEX Game</h1>
     <h1 v-if="start">{{ `RGB: ${r} ${g} ${b}` }}</h1>
-    <input :disabled="inputDisabled" type="text" v-if="start" v-model="message" placeholder="Enter hex code..." />
+    <input
+      :class="{ sucess: message.length == 7, error: message.length < 7 && message.length > 0 }"
+      :disabled="inputDisabled"
+      type="text"
+      v-if="start"
+      maxlength="7"
+      v-model="message"
+      :placeholder="!inputDisabled ? 'Enter hex code...' : ''"
+    />
     <br />
     <button v-if="start && !messageVisible" @click="submit">Submit</button><br />
 
@@ -61,10 +72,58 @@ export default {
       {{ submit() }}
     </p>
   </div>
+  <footer v-if="!start">
+    Made with <span class="heart">❤</span> By
+    <a target="_blank" href="https://github.com/maciekt07">github.com/maciekt07</a>
+  </footer>
 </template>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
+
+.heart {
+  transition: 0.3s;
+}
+.heart:hover {
+  color: #3abdff;
+}
+a {
+  cursor: pointer;
+  transition: 0.4s all;
+  color: #3abdff;
+  display: inline-block;
+  position: relative;
+  text-decoration: none;
+}
+a::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  transform: scaleX(0);
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: #3abdff;
+  transform-origin: bottom right;
+  transition: transform 0.25s ease-out;
+  border-radius: 100px;
+}
+
+a:hover {
+  text-shadow: 0px 0px 20px #3abdff;
+}
+
+a:hover::after {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
+
+footer {
+  width: 100%;
+  color: white;
+  text-align: center;
+  font-size: 20px;
+}
 
 input[type="text"] {
   font-size: 20px;
@@ -72,6 +131,17 @@ input[type="text"] {
   border-radius: 8px;
   border: none;
 }
+
+input[type="text"].error:focus,
+input[type="text"].error:focus-visible {
+  outline: 3px solid red;
+}
+
+input[type="text"].sucess:focus,
+input[type="text"].sucess:focus-visible {
+  outline: 3px solid green;
+}
+
 p {
   background: rgb(77, 77, 77);
   padding: 10px;
@@ -132,7 +202,7 @@ button {
   border-radius: 8px;
   border: 1px solid transparent;
   padding: 0.6em 1.2em;
-  font-size: 1em;
+  font-size: 1.15em;
   font-weight: 500;
   font-family: inherit;
   background-color: #1a1a1a;
